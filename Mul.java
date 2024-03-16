0332ef5c0e95b5a36d1cd1721ccc8f18bfe9cac2
muhsyed22 <149222230+muhsyed22@users.noreply.github.com>
2024-03-16 15:23:51 +0530

    multiplication table

---
+++ Mul.java
@@ -0,0 +1,9 @@
+class Mul{
+         public static void main(String[] ar){
+         System.out.println("multiplication table");
+         int n=Integer.parseInt(ar[0]);
+         for (int i=1;i<=10;i++){
+               System.out.println(n+"*"+i+"="+n*i);
+       }
+  } 
+}
